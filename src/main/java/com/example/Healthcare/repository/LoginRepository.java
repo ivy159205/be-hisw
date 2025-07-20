5be006faa2c3dfd61dc14da3fae0aa943d2a36d7
unknown <haiduc159205.hsr@outlook.com>
2025-07-20 21:56:13 +0700

    backend tới hết metrictype

--- src/main/java/com/example/Healthcare/repository/LoginRepository.java
+++ src/main/java/com/example/Healthcare/repository/LoginRepository.java
@@ -3,6 +3,6 @@ import com.example.Healthcare.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface LoginRepository extends JpaRepository<User, String> {
+public interface LoginRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String username);
 }
